ed06051af442900b20ec9eba16bd7173a3c7020e
JBorchers <79878454+JBorchers@users.noreply.github.com>
2023-10-14 12:13:20 -0600

    update

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'arcane-savannah-13758.herokuapp.com']
 
 
 # Application definition
